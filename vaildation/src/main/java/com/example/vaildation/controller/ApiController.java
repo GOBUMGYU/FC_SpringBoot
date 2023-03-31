f346883fe74657cff78d03ed2406dbbbd87d5240
bumgyu <soul1902@naver.com>
2023-03-31 18:40:52 +0900

    Spring Boot Validation

--- vaildation/src/main/java/com/example/vaildation/controller/ApiController.java
+++ vaildation/src/main/java/com/example/vaildation/controller/ApiController.java
@@ -4,25 +4,42 @@ import com.example.vaildation.dto.User;
 import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
     @PostMapping("/user")
-    public ResponseEntity user(@RequestBody User user) {
+    public ResponseEntity user(@Valid @RequestBody User user, BindingResult bindingResult) {
         System.out.println(user);
 
-        if(user.getPhoneNumber() == "xxx-xxxx-xxxx") {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(user);
+        //Valid 검사에서 에러가 있을 경우 예외처리
+        if(bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            bindingResult.getAllErrors().forEach(objectError -> {
+                FieldError field = (FieldError) objectError;
+                String message = objectError.getDefaultMessage();
+
+                System.out.println("filed : " + field.getField());
+                System.out.println("message = " + message);
+                sb.append("field : " + field.getField());
+                sb.append("message : " + message);
+
+            });
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(sb.toString());
         }
 
-        return
+        //logic
+
+
+        return ResponseEntity.ok(user);
     }
-
-
 }
